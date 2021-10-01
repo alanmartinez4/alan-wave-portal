af30e60dc52805eefb5ae17640f3a8298c6c4a1f
Alan Martinez <18110522+alanmartinez4@users.noreply.github.com>
2021-10-01 00:05:11 -0500

    Update run.js
    storing messages from users on the blockchain

--- scripts/run.js
+++ scripts/run.js
@@ -3,37 +3,38 @@
 // IDEAS: Maybe you want to store the address of the sender in an array? Maybe you want to store a map of addresses and wave counts so you keep track of who's waving at you the most? 
 
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); // compile our contract and generate the necessary files we need to work with our contract under the artifacts directory
-    const waveContract = await waveContractFactory.deploy(); // Hardhat will create a local Ethereum network for us, but just for this contract. Then, after the script completes it'll destroy that local network. So, every time you run the contract, it'll be a fresh blockchain.
-    await waveContract.deployed(); // wait until contract is officially deployed
+  const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
-    console.log("Contract deployed to: ", waveContract.address); // display the address of the contract
-    console.log("Contract deployed by:", owner.address);
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-    console.log("Current amount of waves is %s", waveCount);
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave('A message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+  const [_, randoPerson] = await ethers.getSigners();
+  waveTxn = await waveContract.connect(randoPerson).wave('Another message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-    waveCount = await waveContract.getTotalWaves();
-
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 runMain();
